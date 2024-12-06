d48b85963a100bbcd1ed80e1661fd1b0f831a12d
enriqueman <manzano.velasco217@gmail.com>
2024-12-06 13:49:08 -0500

    [HOTFIX]/ NextConfig map patch

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export',
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      ...defaultPathMap,
+      '/experience/': { page: '/experience' },
+      '/education/': { page: '/education' },
+      '/skills/': { page: '/skills' },
+      '/projects/': { page: '/projects' },
+      '/contact/': { page: '/contact' },
     }
+  },
 };
 
 export default nextConfig;
