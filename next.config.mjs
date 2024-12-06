ba6afbd3207c282c63959eb48db9b1607f113e51
enriqueman <manzano.velasco217@gmail.com>
2024-12-06 13:17:25 -0500

    [HOTFIX]/ NextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,7 @@ const nextConfig = {output: 'export',
     trailingSlash: true,
     images: {
       unoptimized: true,
-    },
-    experimental: {
-     appDir: true,
-   },
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://falconcloud.co/site_srv10_ph/site/api/:path*',
-        },
-      ]
-    },
-    reactStrictMode: true,
-    swcMinify: true,
+    }
 };
 
 export default nextConfig;
